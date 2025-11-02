ec4cdc55bda41c7fd3483f01385f6080b9e41a9d
Anuska Acharya <aanuska58@gmail.com>
2025-11-02 15:08:46 +0000

    Modeled Decision Tree

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
 import pickle
 
 url="https://raw.githubusercontent.com/leontoddjohnson/datasets/refs/heads/main/data/coffee_analysis.csv"
@@ -7,12 +8,9 @@ url="https://raw.githubusercontent.com/leontoddjohnson/datasets/refs/heads/main/
 # df=pd.read_csv(url)
 # df=df.dropna()
 # print(df.info())
-def main():
+def train_model_1():
     # Load dataset
     df = pd.read_csv(url)
-
-    # Preprocess data: drop rows with missing values
-    df = df.dropna()
 
     # Define features and target variable
     X = df[["100g_USD"]]
@@ -23,9 +21,38 @@ def main():
     model.fit(X, y)
 
     # Save the trained model to a file
-    with open("linear_regression_model.pkl", "wb") as f:
+    with open("model1.pkl", "wb") as f:
         pickle.dump(model, f)
-    print("Model trained and saved as linear_regression_model.pkl")
+    print("Model trained and saved as model1.pkl")
 
+def roast_category(value):
+    mapping = {
+        "Light":0,
+        "Medium-Light":1,
+        "Medium":2,
+        "Medium-Dark":3,
+        "Dark":4
+    }
+    return mapping.get(value, None)
+
+def train_model_2():
+    # Load dataset
+    df = pd.read_csv(url)
+
+    df["roast_cat"]=df["roast"].apply(roast_category)
+    # Define features and target variable
+    X = df[["100g_USD", "roast_cat"]]
+    y = df["rating"]
+
+    # Train decision tree regression model
+    model = DecisionTreeRegressor(random_state=42)
+    model.fit(X, y)
+
+    # Save the trained model to a file
+    with open("model2.pkl", "wb") as f:
+        pickle.dump(model, f)
+    print("Model trained and saved as model2.pkl")
+    
 if __name__ == "__main__":
-    main()
+    train_model_1()
+    train_model_2()
